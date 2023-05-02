1c77fba27cab9945fda93d239fc3139d359058cd
moshafieeha <m.shafieeha@gmail.com>
2023-05-02 11:18:00 -0700

    Add login route logic

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -4,28 +4,27 @@ const User = require("../models/user.model");
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
 
+//////////////// Register ////////////////
+router.post("/Register", async (req, res, next) => {
+  try {
+    // const { error } = validateUser(req.body);
+    //if (error) return next(createError(500, error.message));
+    console.log("innnnnnn");
 
-//////////////// Sign up ////////////////
-router.post("/", async (req, res, next) => {
-    try {
-      // const { error } = validateUser(req.body);
-      //if (error) return next(createError(500, error.message));
-        console.log("innnnnnn");
+    // If validation passes, create a new user object and save it to the database
+    const user = new User({
+      fName: req.body.fName,
+      lName: req.body.lName,
+      username: req.body.username,
+      password: req.body.password,
+      gender: req.body.gender,
+      phone: req.body.phone,
+      role: req.body.role,
+    });
 
-      // If validation passes, create a new user object and save it to the database
-      const user = new User({
-        fName: req.body.fName,
-        lName: req.body.lName,
-        username: req.body.username,
-        password: req.body.password,
-        gender: req.body.gender,
-        phone: req.body.phone,
-        role: req.body.role,
-      });
+    console.log(user);
+    await user.save();
 
-      console.log(user);
-      await user.save();
-  
     //   // Save the user data to the session
     //   req.session.user = {
     //     _id: user._id,
@@ -34,13 +33,43 @@ router.post("/", async (req, res, next) => {
     //     lastName: user.lastName,
     //     role: user.role,
     //   };
-  
-      res.status(201).json(user);
-    } catch (err) {
-      return next(createError(500, err.message));
-    }
-  });
-  
 
+    res.status(201).json(user);
+  } catch (err) {
+    return next(createError(500, err.message));
+  }
+});
+
+//////////////// Login ////////////////
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user in the database
+    const user = await User.findOne({ username });
+    if (!user) {
+      return next(createError(400, "Invalid username"));
+    }
+
+    // Validate the password
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return next(createError(400, "Invalid password"));
+    }
+
+    // // Save the user data to the session
+    // req.session.user = {
+    //   _id: user._id,
+    //   username: user.username,
+    //   firstName: user.firstName,
+    //   lastName: user.lastName,
+    //   role: user.role,
+    // };
+
+    res.status(201).json(user);
+  } catch (err) {
+    return next(createError(500, err.message));
+  }
+});
 
 module.exports = router;
